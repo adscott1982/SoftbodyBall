f9753e901755d543f646d97fa9a422524dff5656
Andrew Scott <adscott1982@googlemail.com>
2020-11-05 22:20:25 +0000

    First commit.

---
+++ Assets/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public int NumberOfSides = 4;
+    public float Radius = 1;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Create the sides
+        var positions = this.GetPositions();
+    }
+
+    private List<Vector2> GetPositions(int v)
+    {
+        var origin = this.transform.position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
